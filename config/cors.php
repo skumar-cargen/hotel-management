b7e2fe5a4c6650e48000b455f0e8b7b197279f59
Sagar Kumar <skumar@cargen.co.tz>
2026-04-19 14:54:09 +0500

    added changes

--- config/cors.php
+++ config/cors.php
@@ -2,19 +2,19 @@
 
 return [
 
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['Content-Type', 'Accept', 'Authorization', 'X-Domain', 'X-Requested-With'],
+    'allowed_headers' => ['Content-Type', 'Accept', 'Authorization', 'X-Domain', 'X-Requested-With', 'X-CSRF-TOKEN', 'Origin'],
 
     'exposed_headers' => [],
 
-    'max_age' => 0,
+    'max_age' => 86400,
 
     'supports_credentials' => false,
 
